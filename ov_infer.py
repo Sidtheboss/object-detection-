1557d8c2cf8f57ef0f133899dc627cec234330e6
Siddharth Venkatesh <126441692+Sidtheboss@users.noreply.github.com>
2024-03-07 11:25:49 +0530

    ov_infer.py
    The labels daisy , roses , sunflowers , tulips. This includes openvino,cv2 & numpy.

---
+++ ov_infer.py
@@ -0,0 +1,17 @@
+import openvino as ov
+import cv2
+import numpy as np
+core=ov.Core()
+model=core.read_model('flower.xml')
+compiled_model=core.compile_model(model=model)
+output_key=compiled_model.output(0)
+op='fl.jpg'
+img_op=cv2.imread(op)
+img_op=cv2.resize(img_op,(180,180))
+img_op_n=np.expand_dims(img_op,axis=0)
+
+op=compiled_model(img_op_n)[output_key]
+op=np.argmax(op)
+
+class_labels=['daisy', 'dandelion', 'roses', 'sunflowers', 'tulips']
+print(class_labels[op])
